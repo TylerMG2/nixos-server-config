17dd9ca7c6c7ba877aeb296fea7a90f94afff12b
TylerMG2 <tylermg17@gmail.com>
2025-10-27 01:07:03 +1100

    disabled old service

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,8 @@
     };
   };
 
+  systemd.services.podman-portainer.enable = false;
+
   virtualisation.oci-containers = {
     containers.portainer = {
       image = "docker.io/portainer/portainer-ce:latest";
