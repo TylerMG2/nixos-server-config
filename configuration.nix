c8ff9610d23425584c13de52c9e1e2c2aa6fa52e
TylerMG2 <tylermg17@gmail.com>
2025-10-31 02:38:58 +1100

    Commented out new things

--- configuration.nix
+++ configuration.nix
@@ -137,18 +137,18 @@
     shell = pkgs.bash; #TODO: Remove later\
 
     # Stop podman running out of mappings
-    subUidRanges = [
-      {
-        startUid = 100000;
-        count = 65536;
-      }
-    ];
-    subGidRanges = [
-      {
-        startGid = 100000;
-        count = 65536;
-      }
-    ];
+    # subUidRanges = [
+    #   {
+    #     startUid = 100000;
+    #     count = 65536;
+    #   }
+    # ];
+    # subGidRanges = [
+    #   {
+    #     startGid = 100000;
+    #     count = 65536;
+    #   }
+    # ];
   };
 
   users.groups.podman = {
@@ -179,10 +179,10 @@
     ''d /home/podman/jellyfin/cache 0750 podman podman -''
     ''d /home/podman/jellyfin/media 0750 podman podman -''
 
-    ''d /home/podman/sonarr/config 0750 podman podman -''
-    ''d /home/podman/radarr/config 0750 podman podman -''
-    ''d /home/podman/lidarr/config 0750 podman podman -''
-    ''d /home/podman/prowlarr/config 0750 podman podman -''
+    # ''d /home/podman/sonarr/config 0750 podman podman -''
+    # ''d /home/podman/radarr/config 0750 podman podman -''
+    # ''d /home/podman/lidarr/config 0750 podman podman -''
+    # ''d /home/podman/prowlarr/config 0750 podman podman -''
   ];
 
   # Wireguard VPN for connecting to services
