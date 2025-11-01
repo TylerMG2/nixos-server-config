5dc1f864f1ddc70d8ec70de804c78c61fe6fabcf
TylerMG2 <tylermg17@gmail.com>
2025-11-02 00:52:27 +1100

    Testing more thigns

--- configuration.nix
+++ configuration.nix
@@ -205,9 +205,6 @@
     ''d /home/podman/radarr 0750 podman podman -''
     ''d /home/podman/radarr/config 0750 podman podman -''
 
-    ''d /home/podman/lidarr 0750 podman podman -''
-    ''d /home/podman/lidarr/config 0750 podman podman -''
-
     ''d /home/podman/prowlarr 0750 podman podman -''
     ''d /home/podman/prowlarr/config 0750 podman podman -''
   ];
